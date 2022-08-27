bd9e09befde78765440701816ad4720721cbb33e
Kuldeep115pk <kuldeeppatilpk@gmail.com>
2022-08-27 11:55:28 +0530

    forth commit

--- src/main/java/springjenkins/demo/DemoApplication.java
+++ src/main/java/springjenkins/demo/DemoApplication.java
@@ -13,7 +13,7 @@ public class DemoApplication {
 	public static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 	
 	@PostConstruct
-	public init() {
+	public void init() {
 		logger.info("Application Started...");
 	}
 
